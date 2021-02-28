49a6cdaa417dddbaed5844b0c073c2e4efae9246
Darsh Panchal <16594672+darshpanchal@users.noreply.github.com>
2021-02-28 00:14:49 -0500

    added BST

---
+++ BinarySearchTree.py
@@ -0,0 +1,30 @@
+class BST:
+    def __init__(self,data):
+        self.head = data
+        self.left = None
+        self.right = None
+
+    def inserttree(self, arr):
+        for element in arr:
+            if element < self.head:
+                if self.left is None:
+                    self.left = BST(element)
+                else:
+                    self.left.inserttree([element])
+            else:
+                if self.right is None:
+                    self.right = BST(element)
+                else:
+                    self.right.inserttree([element]) 
+    
+    def gettree(self):
+        if self.left:
+            self.left.gettree()
+        print( self.head),
+        if self.right:
+            self.right.gettree()
+
+if __name__ == "__main__":
+    t = BST(7)
+    t.inserttree([5,2,8,9,6,20,10,15,100,70,30])
+    t.gettree()
